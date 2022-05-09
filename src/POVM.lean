cb3e625960833110782bd958a2858a95d5e817a6
Bassem <25056492+BassemSafieldeen@users.noreply.github.com>
2022-05-09 12:06:12 +0200

    Add POVM.lean

---
+++ src/POVM.lean
@@ -0,0 +1,10 @@
+variables (ℋ : Type) [complex_Hilbert_space ℋ]
+
+/--
+Positive operator-valued measure
+is a set of operators
+that satisfy non-negativity and completeness:
+-/
+class POVM (Λ : ι → (ℋ →ₗ[ℂ] ℋ)) :=
+(ops_nonneg  : ∀ i, Λ i ≥ 0)
+(sum_ops_one : ∑ i, Λ i = 1)
