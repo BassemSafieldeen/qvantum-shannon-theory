0c45af54a51bea0e7463423cf7ac9d8abd441848
Bassem <25056492+BassemSafieldeen@users.noreply.github.com>
2022-05-19 11:11:49 +0200

    start def trace

--- src/Quantum_state.lean
+++ src/Quantum_state.lean
@@ -5,8 +5,10 @@ A Quantum state is a linear operator
 on a complex hilbert space ℋ.
 -/
 variables
-(ℋ : Type) [complex_Hilbert_space ℋ]
+{ℋ : Type} [complex_Hilbert_space ℋ]
 
 class quantum_state (ρ : ℋ →ₗ[ℂ] ℋ) :=
 (trace_one   : 1=1)
 (pos_semidef : 2=2)
+
+def tr (ρ : ℋ →ₗ[ℂ] ℋ) [quantum_state ρ] : ℝ := sorry
