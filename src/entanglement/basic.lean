bc44469ed27b0ad0335b75a670fe7b76feea4550
Bassem <25056492+BassemSafieldeen@users.noreply.github.com>
2023-02-03 12:39:44 +0100

    entanglement

---
+++ src/entanglement/basic.lean
@@ -0,0 +1,5 @@
+import quantum_state
+
+def distillable_entanglement : ℝ := 0
+
+def entanglement_cost : ℝ := 0
---
+++ src/entanglement/entanglement_transformations_are_irreversible.lean
@@ -0,0 +1,7 @@
+import entanglement.basic
+
+/-
+/-- Thm. 1 in https://www.nature.com/articles/s41567-022-01873-9-/
+theorem entanglement_theory_irreversible :
+∃ ρ, distillable_entanglement ρ ≠ entanglement_cost ρ := sorry
+-/
